598805d85393cb90cc5a7c229882539276c4f879
b127 <phil@b127>
2021-01-19 19:36:28 +0800

    update source code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # aws-pycli
 Manage your AWS Infrastructure via Python
+
+Module installation:
+
+pip install click
+pip install python-dotenv
--- ec2/instance.py
+++ ec2/instance.py
@@ -1,22 +1,43 @@
 
+#!/usr/bin/python3
 import boto3, botocore
-import os
+import os, re
 import click
 
 from dotenv import load_dotenv
 from botocore.exceptions import ClientError
 
-# LOAD ENV
-load_dotenv(verbose=True)
+load_dotenv(verbose=True) # LOAD ENV
 
-arn = os.getenv('MY_ARN')
-regions = None
-ec2 = None
+ARN = os.getenv('MY_ARN')
+ROLE_SESSION = os.getenv('MY_ROLE_SESSION')
+regions = ['ap-northeast-2','us-east-1'] # Set your regions here
+
+#ec2 = None
 
 def Session():
-    pass
+    sts = boto3.client('sts')
+    get_sts = sts.assume_role(RoleArn=ARN,RoleSessionName=ROLE_SESSION)
+    return boto3.session.Session(
+        aws_access_key_id=get_sts['Credentials']['AccessKeyId'],
+        aws_secret_access_key=get_sts['Credentials']['SecretAccessKey'],
+        aws_session_token=get_sts['Credentials']['SessionToken']
+    )
 
-@click.group()
+class AliasedGroup(click.Group):
+    def get_command(self, ctx, cmd_name):
+        rv = click.Group.get_command(self, ctx, cmd_name)
+        if rv is not None:
+            return rv
+        matches = [x for x in self.list_commands(ctx)
+                   if x.startswith(cmd_name)]
+        if not matches:
+            return None
+        elif len(matches) == 1:
+            return click.Group.get_command(self, ctx, matches[0])
+        ctx.fail('Too many matches: %s' % ', '.join(sorted(matches)))
+
+@click.group(cls=AliasedGroup)
 def cli():
     '''
     Helper commands for Ec2 management.
@@ -25,32 +46,55 @@ def cli():
 
 @cli.command()
 @click.argument('instance_id')
-def instance_stat(instance_id):
+def status(instance_id):
     '''
-    Get ec2 status:
-        0 : pending
-        16 : running
-        32 : shutting-down
-        48 : terminated
-        64 : stopping
-        80 : stopped
+    Get Instance status
     '''
+    global ec2
     session = Session()
-    ec2 = session.resource()
-    instance = ec2.Instance(instance_id)
-    status = instance.state()
-    return status
-    
+    #ec2 = session.resource('ec2', region_name=region)
+    for region in regions:
+        ec2 = session.resource('ec2', region_name=region)
+        instance = ec2.Instance(instance_id)
+        print(f'searching [{region}]')
+        try:
+            status = instance.state['Name']
+            print(f'status [{status}]\n')
+        except botocore.exceptions.ClientError as error:
+            print(f'status [Not Found]\n')
+            continue
 
+@cli.command()
+@click.argument('instance_id')
+def restart_ec2(instance_id):
+    '''
+    Restart your Instance
+    '''
+    pass
 
-# restart ec2
+@cli.command()
+@click.argument('instance_id')
+def stop_ec2(instance_id):
+    '''
+    Stop your Instance
+    '''
+    pass
 
-# stop ec2
+@cli.command()
+@click.argument('instance_id')
+def start_ec2(instance_id):
+    '''
+    Start your Instance
+    '''
+    pass
 
-# start ec2
-
-# delete ec2
-
+@cli.command()
+@click.argument('instance_id')
+def delete_ec2(instance_id):
+    '''
+    Delete your Instance
+    '''
+    pass
 
 if __name__ == '__main__':
     cli()
